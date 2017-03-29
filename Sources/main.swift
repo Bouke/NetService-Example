f92ae7f5ce9ac6214e6807441537f66782d3e6e0
Bouke Haarsma <bouke@haarsma.eu>
2017-03-29 12:24:03 +0200

    Cleaned up example

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,78 +1,32 @@
+import struct Foundation.Date
 import class Foundation.NSNumber
 import class Foundation.RunLoop
 import NetService
 import Socket
 
-#if os(OSX)
-    import class Foundation.InputStream
-    import class Foundation.OutputStream
-#endif
+var toggle = true
 
-
-// server
-//var hapPointer = PointerRecord(name: "_hap._tcp.local", unique: false, internetClass: 1, ttl: 4500, destination: "Bridge._hap._tcp.local")
-//var hapService = ServiceRecord(name: "Bridge._hap._tcp.local", unique: false, internetClass: 1, ttl: 120, priority: 0, weight: 0, port: 8000, server: "Bouke's iMac._ssh._tcp.local")
-//var hapHost = HostRecord(name: "Bouke's iMac._ssh._tcp.local", unique: false, internetClass: 1, ttl: 120, ip: IPv4("10.0.1.14")!)
-//var hapInfo = TextRecord(name: "Bridge._hap._tcp.local", unique: false, internetClass: 1, ttl: 120, attributes: [
-//    "pv": "1.0", // state
-//    "id": "11:22:33:44:55:66:77:88", // identifier
-//    "c#": "1", // version
-//    "s#": "1", // state
-//    "sf": "1", // discoverable
-//    "ff": "0", // mfi compliant
-//    "md": "Bridge", // name
-//    "ci": "1" // category identifier
-//    ])
-//
-//let msg = Message(header: Header(id: 0, response: true, operationCode: .query, authoritativeAnswer: true, truncation: false, recursionDesired: false, recursionAvailable: false, returnCode: .NOERROR), questions: [], answers: [hapPointer], authorities: [], additional: [hapService, hapHost, hapInfo])
+// MARK: - Browse for services
 
 class MyBrowserDelegate: NetServiceBrowserDelegate {
     func netServiceBrowserWillSearch(_ browser: NetServiceBrowser) {
         print("Will search: \(browser)")
     }
-
+    
     func netServiceBrowser(_ browser: NetServiceBrowser, didNotSearch errorDict: [String : NSNumber]) {
         print("Did not search: \(errorDict)")
     }
-
+    
     public func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
         print("Did find: \(service)")
     }
-
+    
     public func netServiceBrowser(_ browser: NetServiceBrowser, didRemove service: NetService, moreComing: Bool) {
         print("Did remove: \(service)")
     }
-
+    
     public func netServiceBrowserDidStopSearch(_ browser: NetServiceBrowser) {
-
-    }
-}
-
-var toggle = true
-
-class MyServiceDelegate: NetServiceDelegate {
-    func netServiceWillPublish(_ sender: NetService) {
-        print("\(sender) will publish")
-    }
-
-    func netServiceDidPublish(_ sender: NetService) {
-        print("did publish")
-        print("\(sender) did publish")
-    }
-
-    func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
-        print("\(sender) did not publish: \(errorDict)")
-    }
-
-    func netServiceDidStop(_ sender: NetService) {
-        print("\(sender) did stop")
-    }
-
-    public func netService(_ sender: NetService, didAcceptConnectionWith socket: Socket) {
-        print("Did accept connection from \(socket.remoteHostname)")
-        print(try! socket.readString())
-        try! socket.write(from: "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!")
-        socket.close()
+        print("Did stop: \(browser)")
     }
 }
 
@@ -80,13 +34,42 @@ let browser0 = NetServiceBrowser()
 browser0.searchForServices(ofType: "_airplay._tcp.", inDomain: "local.")
 
 let browser1 = NetServiceBrowser()
-//browser1.searchForServices(ofType: "_airport._tcp.", inDomain: "local.")
+browser1.searchForServices(ofType: "_airport._tcp.", inDomain: "local.")
 
 let browserDelegate = MyBrowserDelegate()
 browser0.delegate = browserDelegate
 browser1.delegate = browserDelegate
 
-let ns = NetService(domain: "local.", type: "_airplay._tcp.", name: "demo", port: 8000)
+
+// MARK: - Publish a service
+
+class MyServiceDelegate: NetServiceDelegate {
+    func netServiceWillPublish(_ sender: NetService) {
+        print("Will publish: \(sender)")
+    }
+    
+    func netServiceDidPublish(_ sender: NetService) {
+        print("Did publish: \(sender)")
+    }
+    
+    func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
+        print("Did not publish: \(sender), because: \(errorDict)")
+    }
+    
+    func netServiceDidStop(_ sender: NetService) {
+        print("Did stop: \(sender)")
+    }
+    
+    func netService(_ sender: NetService, didAcceptConnectionWith socket: Socket) {
+        print("Did accept connection: \(sender), from: \(socket.remoteHostname)")
+        print(try! socket.readString() ?? "")
+        try! socket.write(from: "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!")
+        socket.close()
+    }
+}
+
+// Publish example HomeKit device.
+let ns = NetService(domain: "local.", type: "_hap._tcp.", name: "example", port: 8000)
 precondition(ns.setTXTRecord([
     "pv": "1.0", // state
     "id": "11:22:33:44:55:66:77:99", // identifier
@@ -99,10 +82,16 @@ precondition(ns.setTXTRecord([
 ]))
 let serviceDelegate = MyServiceDelegate()
 ns.delegate = serviceDelegate
-ns.publish(options: [.noAutoRename, .listenForConnections])
+ns.publish(options: [.listenForConnections])
+
+
+// MARK: - Manage runloop
 
 withExtendedLifetime((browser0, browser1, browserDelegate, ns, serviceDelegate)) {
-    RunLoop.main.run()
+    if CommandLine.arguments.contains("--test") {
+        print("Running runloop for 10 seconds...")
+        RunLoop.main.run(until: Date(timeIntervalSinceNow: 10))
+    } else {
+        RunLoop.main.run()
+    }
 }
-
-print("Got here!")
